f8ddca17c54d80acf0509bbd672d91e68d4377d6
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-17 17:07:13 +0300

    Add ex1

---
+++ Ex1/index.py
@@ -0,0 +1,21 @@
+class Parallelepiped:
+    def __init__(self, width, length, height):
+        self.width = width
+        self.length = length
+        self.height = height
+
+    def get_volume(self):
+        return self.width * self.height * self.length
+
+    def get_base_square(self):
+        return self.width * self.height
+
+    def get_side_square(self):
+        return 2 * (self.width * self.height + self.length * self.height)
+
+    @classmethod
+    def info(cls):
+        return dir(cls)
+
+figure = Parallelepiped(10, 15, 20)
+print(f"Volume = {figure.get_volume()}, Base square = {figure.get_base_square()}, Side square = {figure.get_side_square()}")
